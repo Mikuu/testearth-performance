5672dbc90e79ccdd48e172a37c2393d9a30a0126
Biao <biaofu@thoughtworks.com>
2024-03-20 16:21:36 +0800

    update

--- k666/src/configs/metric.config.js
+++ k666/src/configs/metric.config.js
@@ -5,27 +5,15 @@ export default {
         errors: new Counter("cm_create_book_errors"),
         requests: new Counter("cm_create_book_reqs"),
         requestDuration: new Trend("cm_create_book_req_duration", true),
-
         requestSending: new Trend("cm_create_book_req_sending", true),
         requestWaiting: new Trend("cm_create_book_req_waiting", true),
         requestReceiving: new Trend("cm_create_book_req_receiving", true),
-    },
-
-    queryBookRequest: {
-        errors: new Counter("cm_query_book_errors"),
-        requests: new Counter("cm_query_book_reqs"),
-        requestDuration: new Trend("cm_query_book_req_duration", true),
-
-        requestSending: new Trend("cm_query_book_req_sending", true),
-        requestWaiting: new Trend("cm_query_book_req_waiting", true),
-        requestReceiving: new Trend("cm_query_book_req_receiving", true),
     },
 
     getBookRequest: {
         errors: new Counter("cm_get_book_errors"),
         requests: new Counter("cm_get_book_reqs"),
         requestDuration: new Trend("cm_get_book_req_duration", true),
-
         requestSending: new Trend("cm_get_book_req_sending", true),
         requestWaiting: new Trend("cm_get_book_req_waiting", true),
         requestReceiving: new Trend("cm_get_book_req_receiving", true),
@@ -35,7 +23,6 @@ export default {
         errors: new Counter("cm_update_book_errors"),
         requests: new Counter("cm_update_book_reqs"),
         requestDuration: new Trend("cm_update_book_req_duration", true),
-
         requestSending: new Trend("cm_update_book_req_sending", true),
         requestWaiting: new Trend("cm_update_book_req_waiting", true),
         requestReceiving: new Trend("cm_update_book_req_receiving", true),
@@ -45,7 +32,6 @@ export default {
         errors: new Counter("cm_delete_book_errors"),
         requests: new Counter("cm_delete_book_reqs"),
         requestDuration: new Trend("cm_delete_book_req_duration", true),
-
         requestSending: new Trend("cm_delete_book_req_sending", true),
         requestWaiting: new Trend("cm_delete_book_req_waiting", true),
         requestReceiving: new Trend("cm_delete_book_req_receiving", true),
