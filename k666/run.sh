f7e12dc3a860f3aea983409f0fd83b9b4b6ab815
Biao <biaofu@thoughtworks.com>
2024-03-25 16:26:27 +0800

    update k6 docs

---
+++ k666/run.sh
@@ -0,0 +1,4 @@
+K6_INFLUXDB_ORGANIZATION=orgariman \
+K6_INFLUXDB_BUCKET=bucketariman \
+K6_INFLUXDB_TOKEN=BLR7a7CT6rtnyMvU9MJJkP728wxvtKbCadbbmipuydQd959iZP9YC5-Xsz_5dziqBnKElkoOj_nCYZQlvWmsyw== \
+./result-visualization/grafana/bin/k6 run -o xk6-influxdb=http://localhost:8086 run.js
