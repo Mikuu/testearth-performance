44f19e6dbe4599052e6f67578ab4e8c9db1658e6
Biao <biaofu@thoughtworks.com>
2024-03-27 16:32:43 +0800

    refactor

--- k666/run2grafana.sh
+++ k666/run2grafana.sh
@@ -1,5 +1,5 @@
 K6_INFLUXDB_ORGANIZATION=orgariman \
 K6_INFLUXDB_BUCKET=bucketariman \
-K6_INFLUXDB_TOKEN=BLR7a7CT6rtnyMvU9MJJkP728wxvtKbCadbbmipuydQd959iZP9YC5-Xsz_5dziqBnKElkoOj_nCYZQlvWmsyw== \
+K6_INFLUXDB_TOKEN=ksxSAcFbXlKmhDfhHrpc5tbMpJp08T0teej8X2mx1Xxne6yIA_5m0Tr95ZCfPAG0rlRhVMFAQTiBXXG68GQJMQ== \
 K6_INFLUXDB_PUSH_INTERVAL=2s \
 ./result-visualization/grafana/bin/k6 run -o xk6-influxdb=http://localhost:8086 run.js
