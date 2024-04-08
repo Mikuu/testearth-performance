56c9891ef3405a87605842333e83f3cb52d8430d
Biao <biaofu@thoughtworks.com>
2024-04-08 12:01:28 +0800

    update dashboards and readme to add query with influxql

--- k666/run2grafana.sh
+++ k666/run2grafana.sh
@@ -1,5 +1,7 @@
+date
 K6_INFLUXDB_ORGANIZATION=orgariman \
 K6_INFLUXDB_BUCKET=bucketariman \
-K6_INFLUXDB_TOKEN=ksxSAcFbXlKmhDfhHrpc5tbMpJp08T0teej8X2mx1Xxne6yIA_5m0Tr95ZCfPAG0rlRhVMFAQTiBXXG68GQJMQ== \
+K6_INFLUXDB_TOKEN=Vts7HN2gQbCChOkFMkVg01ted9avUfqgmb2tUTHX8ceDVzJnvnBtnMNLN84kgMg8_ZmPad5apZWBzDGSb4T2ZA== \
 K6_INFLUXDB_PUSH_INTERVAL=2s \
 ./result-visualization/grafana/bin/k6 run -o xk6-influxdb=http://localhost:8086 run.js
+date
