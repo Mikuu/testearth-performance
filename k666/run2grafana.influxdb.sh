c548c26e540744b71f8091eb68fcc6f832e79d26
Biao <biaofu@thoughtworks.com>
2024-04-16 13:58:49 +0800

    refactor execute script for running k6 and pushing data to influxdb

--- k666/run2grafana.influxdb.sh
+++ k666/run2grafana.influxdb.sh
@@ -3,5 +3,5 @@ K6_INFLUXDB_ORGANIZATION=orgariman \
 K6_INFLUXDB_BUCKET=bucketariman \
 K6_INFLUXDB_TOKEN=Vts7HN2gQbCChOkFMkVg01ted9avUfqgmb2tUTHX8ceDVzJnvnBtnMNLN84kgMg8_ZmPad5apZWBzDGSb4T2ZA== \
 K6_INFLUXDB_PUSH_INTERVAL=2s \
-./result-visualization/grafana/bin/k6 run -o xk6-influxdb=http://localhost:8086 run.js
+./result-visualization/grafana/bin/influxdb/k6 run -o xk6-influxdb=http://localhost:8086 run.js
 date
