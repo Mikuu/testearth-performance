a1fa5604c7f93ffeeba52ee6262b52e532ee4cb8
Biao <biaofu@thoughtworks.com>
2024-04-25 17:10:15 +0800

    add doc for system TCP requests

--- k666/run.js
+++ k666/run.js
@@ -7,9 +7,9 @@ import { deleteBookScenario } from "./src/scenarios/deleteBook.scenario.js";
 import { compositeScenario } from "./src/scenarios/composite.scenario.js";
 
 const stages = [
-    { duration: "10m", target: 5 },
-    // { duration: "15m", target: 15 },
-    // { duration: "15m", target: 30 },
+    { duration: "5m", target: 20000 },
+    { duration: "3m", target: 20000 },
+    { duration: "2m", target: 30 },
     // { duration: "10m", target: 10 },
     // { duration: "5m", target: 5 },
 ];
@@ -22,41 +22,41 @@ export const options = {
             executor: "ramping-vus",
             startVUs: 1,
             stages: stages,
-            gracefulRampDown: "0s",
+            gracefulRampDown: "10s",
             exec: "scn_1_createBook",
         },
 
-        scn_2: {
-            executor: "ramping-vus",
-            startVUs: 1,
-            stages: stages,
-            gracefulRampDown: "0s",
-            exec: "scn_2_getBook",
-        },
+        // scn_2: {
+        //     executor: "ramping-vus",
+        //     startVUs: 1,
+        //     stages: stages,
+        //     gracefulRampDown: "0s",
+        //     exec: "scn_2_getBook",
+        // },
 
-        scn_3: {
-            executor: "ramping-vus",
-            startVUs: 1,
-            stages: stages,
-            gracefulRampDown: "0s",
-            exec: "scn_3_updateBook",
-        },
+        // scn_3: {
+        //     executor: "ramping-vus",
+        //     startVUs: 1,
+        //     stages: stages,
+        //     gracefulRampDown: "0s",
+        //     exec: "scn_3_updateBook",
+        // },
 
-        scn_4: {
-            executor: "ramping-vus",
-            startVUs: 1,
-            stages: stages,
-            gracefulRampDown: "0s",
-            exec: "scn_4_deleteBook",
-        },
+        // scn_4: {
+        //     executor: "ramping-vus",
+        //     startVUs: 1,
+        //     stages: stages,
+        //     gracefulRampDown: "0s",
+        //     exec: "scn_4_deleteBook",
+        // },
 
-        scn_5: {
-            executor: "ramping-vus",
-            startVUs: 1,
-            stages: stages,
-            gracefulRampDown: "0s",
-            exec: "scn_5_composite",
-        },
+        // scn_5: {
+        //     executor: "ramping-vus",
+        //     startVUs: 1,
+        //     stages: stages,
+        //     gracefulRampDown: "0s",
+        //     exec: "scn_5_composite",
+        // },
     },
 };
 
